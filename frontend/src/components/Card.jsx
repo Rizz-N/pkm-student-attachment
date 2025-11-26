bcfb6425cdac1f5cb2e26e6ac93701cd28119322
Alfarizi <arzalfarizi12@gmail.com>
2025-11-26 15:49:23 +0700

    add responsive design on Overview

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -4,12 +4,14 @@ const Card = (props) => {
     <div className={props.container} > 
       <div className={props.top}>
         <p>{props.title}</p>
-        <div className="text-3xl">
+        <div className="text-2xl sm:text-sm md:text-2xl font-medium">
           {props.icon}
         </div>
       </div>
       <div>
-        <h1 className={props.rstyle} >{props.result}</h1>
+        <h1 className={`{props.rstyle} text-3xl sm:text-3xl md:text-4xl mt-9 `} >
+          {props.result}
+        </h1>
       </div>
     </div>
   )
--- frontend/src/pages/Overview.jsx
+++ frontend/src/pages/Overview.jsx
@@ -6,42 +6,46 @@ const Overview = () => {
 
   if (loading) {
         return (
-            <div className="mx-10 flex justify-between gap-4 bg-white p-10 rounded-xl">
+            <div className="mx-4 md:mx-10 grid grid-cols-1 lg:grid-cols-4 gap-4 bg-white p-4 md:p-10 rounded-xl">
                 <Card 
-                    container="bg-gray-200 ring-3 ring-gray-300 p-5 flex-1 rounded-xl shadow-xl shadow-gray-500/50" 
-                    top="flex justify-between items-center" 
-                    title="Total Teacher" 
-                    source="https://via.assets.so/img.jpg?w=50&h=50&bg=dbeafe&f=png" 
-                    rstyle="text-4xl mt-10" 
-                    result="Loading..." 
-                />
-                <Card container="bg-gray-200 ring-3 ring-gray-300 p-5 flex-1 rounded-xl shadow-xl shadow-gray-500/50" 
+                  container="bg-gradient-to-b from-white-500 from-90% to-yellow-600 to-10% ring-3 ring-gray-300 p-5 flex-1 rounded-xl shadow-xl shadow-gray-500/50" 
                   top="flex justify-between items-center" 
-                  title="Teacher Present today" 
-                  source="https://via.assets.so/img.jpg?w=50&h=50&bg=dbeafe&f=png" 
+                  title="Total Teacher" 
+                  icon={<FaChalkboardTeacher/>}
                   rstyle="text-4xl mt-10" 
-                  result="Loading..." />
+                  result="Loading" 
+                  />
                 <Card 
-                    container="bg-gray-200 ring-3 ring-gray-300 p-5 flex-1 rounded-xl shadow-xl shadow-gray-500/50" 
-                    top="flex justify-between items-center" 
-                    title="Total Teacher" 
-                    source="https://via.assets.so/img.jpg?w=50&h=50&bg=dbeafe&f=png" 
-                    rstyle="text-4xl mt-10" 
-                    result="Loading..." 
-                />
-                <Card container="bg-gray-200 ring-3 ring-gray-300 p-5 flex-1 rounded-xl shadow-xl shadow-gray-500/50" 
+                  container="bg-gradient-to-b from-white-500 from-90% to-green-600 to-10% ring-3 ring-gray-300 p-5 flex-1 rounded-xl shadow-xl shadow-gray-500/50" 
                   top="flex justify-between items-center" 
-                  title="Teacher Present today" 
-                  source="https://via.assets.so/img.jpg?w=50&h=50&bg=dbeafe&f=png" 
+                  title="Teacher Present Today" 
+                  icon={<FaUserFriends/>}
                   rstyle="text-4xl mt-10" 
-                  result="Loading..." />
+                  result="Loading" 
+                  />
+                <Card 
+                  container="bg-gradient-to-b from-white-500 from-90% to-indigo-600 to-10% ring-3 ring-gray-300 p-5 flex-1 rounded-xl shadow-xl shadow-gray-500/50" 
+                  top="flex justify-between items-center" 
+                  title="Total Student" 
+                  icon={<FaUserGraduate/>}
+                  rstyle="text-4xl mt-10" 
+                  result="Loading" 
+                  />
+                <Card 
+                  container="bg-gradient-to-b from-white-500 from-90% to-blue-600 to-10% ring-3 ring-gray-300 p-5 flex-1 rounded-xl shadow-xl shadow-gray-500/50" 
+                  top="flex justify-between items-center" 
+                  title="Student Present Today" 
+                  icon={<FaUserFriends/>}
+                  rstyle="text-4xl mt-10" 
+                  result="Loading" 
+                  />
             </div>
         );
     }
 
     if (error) {
         return (
-            <div className="mx-10 flex justify-between gap-4 bg-white p-10 rounded-xl">
+            <div className="mx-4 md:mx-10 grid grid-cols-1 lg:grid-cols-4 gap-4 bg-white p-4 md:p-10 rounded-xl">
                 <Card 
                     container="bg-gray-200 ring-3 ring-gray-300 p-5 flex-1 rounded-xl shadow-xl shadow-gray-500/50" 
                     top="flex justify-between items-center" 
@@ -78,30 +82,30 @@ const Overview = () => {
 
   return (
     <>
-        <div className="mx-10 flex justify-between gap-4 bg-white p-10 rounded-xl ">
+        <div className="mx-4 md:mx-10 grid grid-cols-1 lg:grid-cols-4 gap-4 bg-white p-4 md:p-10 rounded-xl">
             <Card container="bg-gradient-to-b from-white-500 from-90% to-yellow-600 to-10% ring-3 ring-gray-300 p-5 flex-1 rounded-xl shadow-xl shadow-gray-500/50" 
                   top="flex justify-between items-center" 
                   title="Total Teacher" 
                   icon={<FaChalkboardTeacher/>}
-                  rstyle="text-4xl mt-10" 
+                   
                   result={total.toString()} />
             <Card container="bg-gradient-to-b from-white-500 from-90% to-green-600 to-10% ring-3 ring-gray-300 p-5 flex-1 rounded-xl shadow-xl shadow-gray-500/50" 
                   top="flex justify-between items-center" 
-                  title="Teacher Present today" 
+                  title="Teacher Present Today" 
                   icon={<FaUserFriends/>} 
-                  rstyle="text-4xl mt-10" 
+                   
                   result={totalGuruHadir.toString()} />
             <Card container="bg-gradient-to-b from-white-500 from-90% to-indigo-600 to-10% ring-3 ring-gray-300 p-5 flex-1 rounded-xl shadow-xl shadow-gray-500/50" 
                   top="flex justify-between items-center" 
                   title="Total Student" 
                   icon={<FaUserGraduate/>}
-                  rstyle="text-4xl mt-10" 
+                   
                   result={totalMurid.toString()} />
             <Card container="bg-gradient-to-b from-white-500 from-90% to-blue-600 to-10% ring-3 ring-gray-300 p-5 flex-1 rounded-xl shadow-xl shadow-gray-500/50" 
                   top="flex justify-between items-center" 
-                  title="Student Present today" 
+                  title="Student Present Today" 
                   icon={<FaUserFriends/>}
-                  rstyle="text-4xl mt-10" 
+                   
                   result={totalMuridHadir.toString()} />
         </div>
     </>
