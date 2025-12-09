bda10c63d038a34872277c8b5e3fea7af5d4dff4
Alfarizi <arzalfarizi12@gmail.com>
2025-12-09 13:33:44 +0700

    revisi nama

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://pkm-student-attachment.vercel.app/",
+      "https://pkm-student-attachment.vercel.app",
     ],
     credentials: true,
   })
